a08fc8ca21a22bf2f297017988637b99c7e383cd
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2017-12-28 16:48:00 +0530

    version upgrade

--- scripts/today-schedule-controller.js
+++ scripts/today-schedule-controller.js
@@ -113,117 +113,146 @@ msfReportsApp
 
 
         }
+
+        var getAjaxData =  function(url){
+          return $.get(url);
+        };
         var finalKeyMap = [];
         var tempMap = [];
-        var getDataValues =  function(keyMap,program){
-          var tei= "";
-
-          $.ajax({
-                    async: false,
-                    type: "GET",
-                    url: "../../enrollments.json?ou="+$scope.selectedOrgUnit.id+"&program="+ program.id +"&programStartDate="+ $scope.startdateSelected +"&programEndDate="+ $scope.enddateSelected +"&skipPaging=true",
-                    success: function (data0) {
-                      for(var g=0;g<data0.enrollments.length;g++){
-                        tei = data0.enrollments[g].trackedEntityInstance;
-                        document.getElementById('perc').innerHTML = (g/data0.enrollments.length)*100;
-
-          $.ajax({
-                    async: false,
-                    type: "GET",
-                    url: "../../trackedEntityInstances/"+ tei +".json?fields=*",
-                    success: function (data) {
-                          for(var e=0;e<data.attributes.length;e++){
-                            var count = keyMap[data.attributes[e].attribute];
-                            tempMap[count] = data.attributes[e].value;
-                          }
-                          // tempMap = finalKeyMap;
-                      $.ajax({
-                                async: false,
-                                type: "GET",
-                                url: "../../events.json?trackedEntityInstance="+tei+"&order=eventDate:ASC",
-                                success: function (data5) {
-
-                                  for(var m=0;m<data5.events.length;m++){
-                                    var finalKeyMap = tempMap;
-
-                                    var newRow = "<tr>";
-                                    for(var n=0;n<data5.events[m].dataValues.length;n++){
-                                      var count2 = keyMap[data5.events[m].dataValues[n].dataElement];
-                                      finalKeyMap[count2] = data5.events[m].dataValues[n].value;
-                                    }
-
-
-                                    for(var h=1;h<keyMap2.length;h++){
-                                      if(finalKeyMap.hasOwnProperty(h)){
-                                        newRow =  newRow + "<td>"+finalKeyMap[h]+"</td>";
-                                      }
-                                      else{
-                                        newRow =  newRow + "<td></td>";
-                                      }
-                                    }
-                                    $('.reporttable').append(newRow + "</tr>");
-                                  }
-
-                                  }
-
-                              });
-                          }
-
-
-                  });
-                }
-
-                document.getElementById('loader').style.display = "none";
-              }
-              });
-        };
-
         var keyMap = [];
 		    var keyMap2 = [];
         var json = "";
+        var flag = "";
+        var flagCount = "";
+        var programid =  "";
+
+
+        var getEvents =  function(tei){
+
+
+          var url1 = "../../events.json?trackedEntityInstance="+tei+"&order=eventDate:ASC";
+          $.when(getAjaxData(url1)).done(function(data5){
+
+
+            $.each( data5.events,function(m,values){
+              var finalKeyMap = tempMap;
+
+              var newRow = "<tr>";
+
+              $.each(data5.events[m].dataValues,function(n,values){
+                var value = data5.events[m].dataValues[n].value;
+                if(value == "true"){value = "Yes"}
+                if(value == "false"){value  = "No"}
+                var count2 = keyMap[data5.events[m].dataValues[n].dataElement];
+                finalKeyMap[count2] = value;
+              });
+
+
+              for(var h=1,arrLen3 = keyMap2.length;h<arrLen3;h++){
+                if(finalKeyMap.hasOwnProperty(h)){
+                  newRow =  newRow + "<td>"+finalKeyMap[h]+"</td>";
+                }
+                else{
+                  newRow =  newRow + "<td></td>";
+                }
+              }
+              $('.reporttable').append(newRow + "</tr>");
+              document.getElementById('loader').style.display = "none";
+            });
+
+                  });
+
+
+        };
+
+        var getTeis =  function(tei){
+
+          var url1 = "../../trackedEntityInstances/"+ tei +".json?fields=*";
+          $.when(getAjaxData(url1)).done(function(data){
+
+
+            $.each(data.attributes,function(e,values){
+                                          var count = keyMap[data.attributes[e].attribute];
+                                          var value = data.attributes[e].value
+                                          if(value == "true"){value = "Yes"}
+                                          if(value == "false"){value  = "No"}
+                                          tempMap[count] = value;
+            });
+                  });
+
+        };
+
+        var getDataValues =  function(keyMap,program){
+          var tei= "";
+
+          var url1 = "../../enrollments.json?ou="+$scope.selectedOrgUnit.id+"&program="+ program.id +"&programStartDate="+ $scope.startdateSelected +"&programEndDate="+ $scope.enddateSelected +"&skipPaging=true";
+          $.when(getAjaxData(url1)).done(function(data0){
+
+              $.each(data0.enrollments,function(g,value){
+                tei = data0.enrollments[g].trackedEntityInstance;
+                flag = data0.enrollments.length;
+                flagCount = g;
+                document.getElementById('perc').innerHTML = parseInt((flagCount/flag)*100) + "%";
+                getTeis(tei);
+                getEvents(tei);
+              });
+
+              });
+        };
+
   var getRows = function(row,hr,program,index){
     //$('.reporttable').append(row);
     //var row = "";
+
     $.ajax({
-              async: false,
-              type: "GET",
-              url: "../../programs/"+ program.id +".json?fields=programStages[id,name]",
-              success: function (data2) {
+      async: false,
+      type: "GET",
+      dataType: "json",
+      contentType: "application/json",
+      url: "../../programs/"+ program.id +".json?fields=programStages[id,name]",
+       success: function (data2) {
                 json = data2;
-      for(var j=0;j<data2.programStages.length;j++){
+                var flag =0;
+    //  for(var j=0, arrLen = data2.programStages.length;j<arrLen;j++){
+      for(var j=0,arrLen1=data2.programStages.length;j<arrLen1;j++){
+          // getHeaderRow(json,program, row, hr, index, j);
+          $.ajax({
+            async: false,
+            type: "GET",
+            dataType: "json",
+            contentType: "application/json",
+            url: "../../programStages/"+data2.programStages[j].id+".json?fields=programStageDataElements[dataElement[id,name]]",
+             success: function (data3) {
+          //  console.log(j);
+            hr = hr + "<th colspan ='"+ (data3.programStageDataElements.length) +"'>"+ json.programStages[j].name +"</th>";
 
-        $.ajax({
-                  async: false,
-                  type: "GET",
-                  url: "../../programStages/"+data2.programStages[j].id+".json?fields=programStageDataElements[dataElement[id,name]]&order=displayName:ASC",
-                  success: function (data3) {
-                    hr = hr + "<th colspan ='"+ (data3.programStageDataElements.length) +"'>"+ json.programStages[j].name +"</th>";
-          for(var k=0;k<data3.programStageDataElements.length;k++){
-            var nameDe = data3.programStageDataElements[k].dataElement.name;
-            var idDe = data3.programStageDataElements[k].dataElement.id;
-            row = row + "<th class='rows' id='"+idDe+"'>"+nameDe+"</th>";
-            keyMap[idDe] = index;
-            keyMap2[index] = idDe;
-            index++;
-          }
+            for(var k=0, arrLen = data3.programStageDataElements.length;k<arrLen;k++){
+              var nameDe = data3.programStageDataElements[k].dataElement.name;
+              var idDe = data3.programStageDataElements[k].dataElement.id;
+              row = row + "<th class='rows' id='"+idDe+"'>"+nameDe+"</th>";
+              keyMap[idDe] = index;
+              keyMap2[index] = idDe;
+              index++;
+            }
 
-          if(j == data2.programStages.length-1){
-                $('.reporttable').append(hr + "</tr>" + row + "</tr>");
-                console.log(keyMap2);
-               console.log(keyMap);
-               console.log(Object.keys(keyMap2).length);
-               console.log(Object.keys(keyMap).length);
-                getDataValues(keyMap,program);
-          }
+                if(j == json.programStages.length-1){
+                      $('.reporttable').append(hr + "</tr>" + row + "</tr>");
+                      console.log(keyMap2);
+                     console.log(keyMap);
+                     console.log(Object.keys(keyMap2).length);
+                     console.log(Object.keys(keyMap).length);
+                      getDataValues(keyMap,program);
+                }
+              }
+          });
         }
-        });
-      }
-    }
+}
     });
 //        return keyMap;
   };
 
   $scope.generateReport = function(program){
+    programid = program.id;
 
     $(".reporttable tbody").remove();
 $(".reporttable tbody").detach();
@@ -233,17 +262,14 @@ $(".reporttable tbody").detach();
       var row =  "<tr>";
       var json = "";
       var index = 1;
-      $.ajax({
-                async: true,
-                type: "GET",
-                url: "../../programs/"+program.id+".json?fields=programTrackedEntityAttributes",
-                success: function (data1) {
+      var url = "../../programs/"+program.id+".json?fields=programTrackedEntityAttributes";
+      $.when(getAjaxData(url)).done(function(data1){
           //json =  data2;
-        for(var i=0;i<data1.programTrackedEntityAttributes.length;i++){
-          keyMap[data1.programTrackedEntityAttributes[i].id] = index;
-          keyMap2[index] = data1.programTrackedEntityAttributes[i].id;
+        for(var i=0,arrayLength=data1.programTrackedEntityAttributes.length;i<arrayLength;i++){
+          keyMap[data1.programTrackedEntityAttributes[i].trackedEntityAttribute.id] = index;
+          keyMap2[index] = data1.programTrackedEntityAttributes[i].trackedEntityAttribute.id;
           index++;
-          row = row + "<th class='rows' id='"+ data1.programTrackedEntityAttributes[i].id +"'>"+ data1.programTrackedEntityAttributes[i].displayName +"</th>";
+          row = row + "<th class='rows' id='"+ data1.programTrackedEntityAttributes[i].trackedEntityAttribute.id +"'>"+ data1.programTrackedEntityAttributes[i].displayName +"</th>";
         }
         //console.log(keyMap);
         //console.log(keyMap2);
@@ -260,7 +286,6 @@ $(".reporttable tbody").detach();
         //      }
 
         //    });
-}
             });
 
   };
