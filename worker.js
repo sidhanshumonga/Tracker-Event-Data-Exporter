3a9fcae0d41e500d401a4bd10b394de61fbb5eca
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2017-12-29 16:26:50 +0530

    thread file :worker

---
+++ worker.js
@@ -0,0 +1,19 @@
+
+self.addEventListener('message',function(e){
+
+  var data = JSON.stringify(e.data);
+  var req = new XMLHttpRequest();
+
+  req.open('GET', e.data, false);
+  req.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+  req.onreadystatechange = function () {
+  if (req.readyState == 4 && req.status == 200) {
+  //self.postMessage({ 'Error': 'No', 'Message': 'Save Successful' });
+  }
+  }
+  req.send();
+  var string = req.response + "&&&" + e.data;
+  self.postMessage(string);
+
+
+},false);
