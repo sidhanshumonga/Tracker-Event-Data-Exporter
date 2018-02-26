8a2371cfbd0b3d930c8291cc22a53c25c134f470
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-02-26 12:08:18 +0530

    msf specific code added

--- scripts/home-controller.js
+++ scripts/home-controller.js
@@ -391,19 +391,24 @@ msfReportsApp.directive('calendar', function () {
   
       };
   
-      var getTeiData = function (eventsAtrr) {
-        var temprMap = [];
-        for (var r = 0, lenn = eventsAtrr.length; r < lenn; r++) {
-          var valuess = eventsAtrr[r];
-          var count = keyMap[valuess.attribute];
-          var value = valuess.value;
-          var optionValue = optionSetArr[value];
-          if (typeof optionValue === undefined || optionValue === undefined) { }
-          else { var value = optionValue; }
-          temprMap[count] = value;
-        }
-        return JSON.parse(JSON.stringify(temprMap));;
-      };
+     
+    var getTeiData = function (eventsAtrr) {
+      var temprMap = [];
+      for (var r = 0, lenn = eventsAtrr.length; r < lenn; r++) {
+        var valuess = eventsAtrr[r];
+        var count = keyMap[valuess.attribute];
+       
+if(valuess.attribute == "qak2Z7cCMpD" || valuess.attribute == "ezNf2g94ycZ" || valuess.attribute == "FzXnQEnYFa5"){
+var value = "PRIVATE";
+}
+else{var value = valuess.value;}
+        var optionValue = optionSetArr[value];
+        if (typeof optionValue === undefined || optionValue === undefined) { }
+        else { var value = optionValue; }
+        temprMap[count] = value;
+      }
+      return JSON.parse(JSON.stringify(temprMap));;
+    };;
   
       var teiResponse;
       var terminateWork = false;
